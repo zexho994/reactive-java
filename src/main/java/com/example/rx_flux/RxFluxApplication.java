be056b6dba40265e23231ecf8add60864bc78132
zexho <zouzhihao994@gmail.com>
2021-08-23 08:49:17 +0800

    init

---
+++ src/main/java/com/example/rx_flux/RxFluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.rx_flux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxFluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RxFluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rx_flux/RxFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rx_flux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RxFluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
